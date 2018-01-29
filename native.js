9af8bb4cc0174ee904c8288c635ac5b9b75ad797
rizalmovic <rizalmovic@gmail.com>
2018-01-29 16:13:13 +0700

    update link on importing components

--- native.js
+++ native.js
@@ -1,9 +1,9 @@
-import Body from './lib/native/components/body.js';
-import Loop from './lib/native/components/loop.js';
-import Sprite from './lib/native/components/sprite.js';
-import Stage from './lib/native/components/stage.js';
-import TileMap from './lib/native/components/tile-map.js';
-import World from './lib/native/components/world.js';
+import Body from './src/native/components/body.js';
+import Loop from './src/native/components/loop.js';
+import Sprite from './src/native/components/sprite.js';
+import Stage from './src/native/components/stage.js';
+import TileMap from './src/native/components/tile-map.js';
+import World from './src/native/components/world.js';
 
 export {
   Body,
